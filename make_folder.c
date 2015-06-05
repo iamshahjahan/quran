ae0316b4a4f3ca5b76aedc0eb7989fb89dbb525f
Khan <khan@khan-AOD270.(none)>
2015-06-05 10:53:40 +0530

    first commit

--- make_folder.c
+++ make_folder.c
@@ -4,19 +4,31 @@
 
 int main()
 {
-	FILE *fp;
+	FILE *fp,*fp1;
 	int i;
 
 	for ( i = 1 ; i < 115 ; i++ )
 	{
 		char file_name[100];
+		char file_name1[100];
 		char index[4];
 		sprintf(index, "%d", i);
 		// strcpy(file_name,"quran_original/");
-		strcpy(file_name,"quran_with_item_tag/");
+		strcpy(file_name,"quran_original/");
+		strcpy(file_name1,"quran/");
 		strcat(file_name,index);
-		fp = fopen(file_name,"w+");
-		
+		fp = fopen(file_name,"r");
+		fp1 = fopen(file_name1,"w+");
+
+		while( fgets(line,10000,fp) != NULL )
+		{
+			if ( strcmp(line,"\n") == 0 || strcmp(line,"Surah Index | NobleQuran.com | Dar-us-Salam.com | Khutba.org | SahihMuslim.org") || strcmp(line,"\n") )
+			{
+				continue;
+			}
+			
+			fputs(changed,fp2);
+		}		
 		// fp = fopen("quran_with_item_tag/file_name","w+");
 
 	}
